3b63df447dff094472b2bd2ef10ed8e625a6b7fb
SYAAINN <alswo08319@gmail.com>
2024-05-28 10:28:21 +0900

    feat: 7주차 세미나 실습

--- app/src/main/java/com/sopt/recomposition/optimization/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/recomposition/optimization/ui/home/HomeScreen.kt
@@ -1,5 +1,12 @@
 package com.sopt.recomposition.optimization.ui.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,17 +20,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -31,82 +44,102 @@ import com.sopt.recomposition.optimization.model.Memo
 import com.sopt.recomposition.optimization.model.memos
 import com.sopt.recomposition.optimization.ui.theme.MemoAppTheme
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HomeScreen() {
-    MemoAppTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            val memoList = remember { memos }
+    var helloWorldVisible by remember { mutableStateOf(true) }
+    var isRed by remember { mutableStateOf(false) }
 
-            Column {
-                AddMemo(memoList)
-                MemoList(memoList)
-            }
-        }
-    }
-}
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isRed) Color.Red else Color.White
+    )
 
-@Composable
-fun AddMemo(memoList: SnapshotStateList<Memo>) {
-    val inputValue = remember { mutableStateOf("") }
-
-    Row(
+    Column(
         modifier = Modifier
-            .padding(all = 16.dp)
-            .height(100.dp),
-        horizontalArrangement = Arrangement.End
+            .padding(16.dp)
+            .background(backgroundColor)
     ) {
-        TextField(
-            modifier = Modifier
-                .fillMaxHeight()
-                .weight(1f),
-            value = inputValue.value,
-            onValueChange = { textFieldValue -> inputValue.value = textFieldValue }
-        )
-        Button(
-            onClick = {
-                memoList.add(
-                    index = 0,
-                    Memo(memoList.size, inputValue.value)
-                )
-                inputValue.value = ""
-            },
-            modifier = Modifier
-                .wrapContentWidth()
-                .fillMaxHeight()
-        ) {
-            Text("ADD")
-        }
-    }
-}
 
-@Composable
-fun ColumnScope.MemoList(memoList: SnapshotStateList<Memo>) {
-    LazyColumn(
-        modifier = Modifier
-            .weight(1f)
-    ) {
-        items(
-            items = memoList
-        ) { memo ->
-            Card(
-                modifier = Modifier
-                    .height(100.dp)
-                    .background(Color.White)
-                    .padding(
-                        horizontal = 16.dp,
-                        vertical = 8.dp
-                    )
-                    .fillMaxWidth(),
-                backgroundColor = Color.LightGray,
-            ) {
-                Text(
-                    text = memo.text,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
+        AnimatedVisibility(
+            visible = helloWorldVisible,
+            enter = scaleIn() + slideInHorizontally(),
+            exit = slideOutVertically() + fadeOut()
+        ) {
+            Text(text = "Hello World!")
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = helloWorldVisible,
+                onClick = { helloWorldVisible = true }
+            )
+            Text(
+                text = "Hello World 보이기"
+            )
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = !helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !helloWorldVisible,
+                onClick = { helloWorldVisible = false }
+            )
+            Text(
+                text = "Hello World 감추기"
+            )
+        }
+
+        Text(text = "배경 색을 바꾸어봅시다.")
+
+        Row(
+            Modifier.selectable(
+                selected = !isRed,
+                onClick = {
+                    isRed = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !isRed,
+                onClick = { isRed = false }
+            )
+            Text(
+                text = "흰색"
+            )
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = isRed,
+                onClick = {
+                    isRed = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = isRed,
+                onClick = { isRed = true }
+            )
+            Text(
+                text = "빨간색"
+            )
         }
     }
 }
